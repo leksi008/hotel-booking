02a49e1cfe6a621bf5b513d4a37b0aacab787239
leksi008 <alyoshka008@gmail.com>
2026-03-06 18:02:45 +0300

    feat(api): add new API endpoints and update existing endpoints

--- src/users/router.py
+++ src/users/router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from starlette.responses import Response
 
+from exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
 from src.users.auth import get_password_hash, authenticate_user, create_access_token
+from src.users.dependencies import get_current_user
+from src.users.models import Users
 from src.users.schemas import SUserAuth
 from src.users.service import UsersService
 
@@ -11,11 +14,11 @@ router = APIRouter(
 )
 
 
-@router.post("/register")
+@router.post("/register", status_code=201)
 async def register_user(user_data: SUserAuth):
     existing_user = await UsersService.find_one_or_none(email=user_data.email)
     if existing_user:
-        raise HTTPException(status_code=500)
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersService.add(email=user_data.email, hashed_password=hashed_password)
 
@@ -24,7 +27,27 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectEmailOrPasswordException()
     access_token = create_access_token({"sub": str(user.id)})
-    response.set_cookie("booking_access_token", access_token, httponly=True)
-    return access_token
+    response.set_cookie(
+        key="booking_access_token",
+        value=access_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=3600
+    )
+    return {"status": "ok"}
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie(
+        key="booking_access_token"
+    )
+    return {"status": "ok"}
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
